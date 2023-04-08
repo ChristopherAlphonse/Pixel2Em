0b1b5e9506d37993446566ae4567e30ed550e374
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-04-08 13:16:47 -0400

    hope this works

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({ tsDecorators: true }),
     VitePWA({
       manifest: {
         name: 'Pixel2Em',
